efe02b4f3398b71ed2e59281a3dd08d705d14f16
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2023-03-27 15:05:32 +0530

    add click to copy

--- src/components/SloganCard.tsx
+++ src/components/SloganCard.tsx
@@ -1,10 +1,16 @@
 export const SloganCard = ({ title = "" }) => {
   return (
     <div
+      onClick={() => {
+        navigator.clipboard.writeText(title);
+      }}
       style={{ backgroundColor: "#F2F2F2" }}
-      className="px-4 py-2 cursor-pointer text-slate-900 h-16 flex items-center"
+      className="px-4 py-2 cursor-pointer text-slate-900 h-16 flex items-center group relative"
     >
-      {title}
+      <span className="">{title}</span>
+      <span className="group-hover:opacity-100 transition-opacity bg-gray-800 text-sm text-gray-100 rounded px-2 py-1 absolute -left-32 -top-6 translate-y-full opacity-0 m-4 mx-auto">
+        Click to copy
+      </span>
     </div>
   );
 };
